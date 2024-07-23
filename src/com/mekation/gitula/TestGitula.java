8269de831540d2b5814f551b15decfe4ca1cad2b
André <anderckom5@hotmail.com>
2024-07-23 16:11:26 +0200

    first commint sans gitignire

---
+++ src/com/mekation/gitula/TestGitula.java
@@ -0,0 +1,18 @@
+package com.mekation.gitula;
+
+public class TestGitula {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String nom = "Salva";
+		
+		if(nom == "Salva") {
+			System.out.println("KITOKO");
+		}else{
+			System.out.println("zero");
+		}
+
+	}
+
+}
